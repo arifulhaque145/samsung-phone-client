33c3c9a629a99c347299804b098f3c578ddbbb1b
Ariful145 <arifulh145@gmail.com>
2021-11-13 07:49:09 +0600

    order

--- src/Main/Pages/Dashboard/Components/User/Order/Order.js
+++ src/Main/Pages/Dashboard/Components/User/Order/Order.js
@@ -2,7 +2,7 @@ import { Button, Grid, Typography } from "@mui/material";
 import React from "react";
 
 function Order({ data }) {
-  const { _id, item, status, name } = data;
+  const { _id, item, status } = data;
 
   const handleDelete = (id) => {
     const check = window.confirm("Do you want to delete?");
@@ -15,13 +15,13 @@ function Order({ data }) {
       columns={{ xs: 4, sm: 8, md: 12 }}
       sx={{ boxShadow: 2, my: 2, p: 2 }}
     >
-      <Grid item xs={2} sm={4} md={3} sx={{pt: 1}}>
+      <Grid item xs={2} sm={4} md={3} sx={{ pt: 1 }}>
         <Typography variant="body1">{_id}</Typography>
       </Grid>
-      <Grid item xs={2} sm={4} md={3} sx={{pt: 1}}>
+      <Grid item xs={2} sm={4} md={3} sx={{ pt: 1 }}>
         <Typography variant="body1">{item}</Typography>
       </Grid>
-      <Grid item xs={2} sm={4} md={3} sx={{pt: 1}}>
+      <Grid item xs={2} sm={4} md={3} sx={{ pt: 1 }}>
         <Typography
           variant="body1"
           sx={{
